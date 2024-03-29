bbc8096d18ec7edd950da62ae5294a89be58803a
Prateek Walia <73119088+prateekwalia31@users.noreply.github.com>
2024-03-29 16:59:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,65 @@ def test_db_connection():
         if conn is not None:
             conn.close()
 
+@app.route('/predict/patient/<patient_id>', methods=['GET'])
+def predict_by_patient_id(patient_id):
+    cursor = None
+
+    try:
+        logger.debug('Start of prediction process')
+        # Connect to the MySQL database
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Fetch atrial fibrillation data from the atrial_fibrillation table based on patient_id
+        cursor.execute(f"SELECT Age, Gender, Channel_1, Channel_2, Channel_3, Channel_4, Channel_5, Channel_6, Channel_7, Channel_8, Channel_9, Channel_10, Channel_11, Channel_12 FROM patient_data WHERE patient_id = {patient_id}")
+        patient_data = cursor.fetchone()
+        logging.debug('Cursor ',patient_data)
+
+        if not patient_data:
+            return jsonify({'error': 'Patient data not found'}), 404
+
+        age, gender, *channels = patient_data
+        # Ensure that 'male' is 1 if Gender is 'Male', else 0, adjusting for your model's requirements
+        sex = 1 if gender == 'Male' else 0
+
+        # Initialize features with age and sex in correct order
+        features = [age, sex]
+
+        # Extract features from each channel's JSON data
+        for channel_json in channels:
+            channel_data = json.loads(channel_json)
+            features.extend([channel_data['SDNN'][0], channel_data['RMSSD'][0], channel_data['PNN50'][0], channel_data['Mean_RR'][0]])
+
+        # Prediction
+        logger.debug('Stating pred')
+        prediction = model.predict([features])[0]
+        # Assuming your model's output maps directly to these labels
+        logger.debug('Finish pred')
+        prediction_label = {
+            0: 'Atrial Fibrillation',
+            1: 'Sinus Rhythm',
+            2: 'Various Arrhythmias'
+        }.get(prediction, 'Unknown')
+
+        return jsonify({
+            'patient_id': patient_id,
+            'prediction': prediction_label,
+            'features': {
+                'age': age,
+                'sex': gender
+            }
+        })
+    
+    except mysql.connector.Error as err:
+        logger.debug('Found an error')
+        return jsonify({'error': f'Database error: {err}'}), 500
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
 if __name__ == '__main__':
     app.run(debug=True)
